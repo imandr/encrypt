e3e00915e95a7b46d18dd35ad099f367ebfefd38
Igor <igorvm@igors-imac.home>
2022-03-27 11:04:41 -0400

    .

--- generate.py
+++ generate.py
@@ -1,26 +1,44 @@
-import secrets, string, sys, getopt
+import secrets, string, sys, getopt, math, os
 
 Usage = """
 python generate.py [options]
-    -n <length>             -- password length, default: 10
+    -n <length>             -- password length, default: 10 characters or 4 words, if -w was used
     -s                      -- do not allow special characters
+    -w                      -- generate password as a sequence of words from the common vocabulary
 """
 
-opts, args = getopt.getopt(sys.argv[1:], "sn:?h")
+opts, args = getopt.getopt(sys.argv[1:], "vsn:?hw")
 opts = dict(opts)
 
 if "-?" in opts or "-h" in opts:
     print(Usage)
     sys.exit(2)
 
-chars = string.ascii_letters + string.digits
-if "-s" not in opts:
-    chars = chars + string.punctuation
-length = int(opts.get("-n", 10))
 
+if "-w" in opts:
+    length = int(opts.get("-n", 3))
+    voc_file = "/usr/share/dict/words"
+    if not os.path.isfile(voc_file):
+        print(f"Vocabulary file {voc_file} not found")
+        sys.exit(1)
+    vocabulary = open(voc_file, "r")
+    words = set(w.strip().lower() for w in vocabulary)
+    words = list(w for w in words if w)
+    combinations = len(words)**length
+    password = "-".join([secrets.choice(words) for _ in range(length)])
+else:
+    length = int(opts.get("-n", 10))
+    chars = string.ascii_letters + string.digits
+    if "-s" not in opts:
+        chars = chars + string.punctuation
+    chars = [c for c in chars if c not in '"#&*<>[]`{}']         # not safe chars
+    combinations = len(chars)**length
+    password = "".join([secrets.choice(chars) for _ in range(length)])
 
-chars = [c for c in chars if c not in '"#&*<>[]`{}']         # not safe chars
+if "-v" in opts:
+    log10 = int(math.log(combinations)/math.log(10))
+    t = int(combinations/3.0e13/2+0.5)          # average time to crack password at the rate of 10**6 passwords per second
+    print(f"Password strength: {combinations} (>10**{log10}) combinations, or {t:.1e} years of cracking")
 
-password = "".join([secrets.choice(chars) for _ in range(length)])
 print(password)
 
--- sha256.py
+++ sha256.py
@@ -3,9 +3,10 @@ import sys, getopt, os
 from datetime import datetime
 
 Usage = """
-python sha256.py <file> ...                 - calculate checksum for one or more files
-python sha256.py -c <hex checksum> <file>   - check checksum for a file
-python sha256.py -c <file with paths and checksums>   - check checksum for multiple files
+python sha256.py [-u <checksums file>] [<file> ...]  - calculate checksum for one or more files
+    -u                                               - update checksums of the given files
+python sha256.py -v <checksums file>                 - check checksum for multiple files
+python sha256.py -c <hex checksum> <file>            - check checksum for a file
 """
 
 def checksum(path):
@@ -18,52 +19,77 @@ def checksum(path):
     
     return h.hexdigest()
 
-opts, args = getopt.getopt(sys.argv[1:], "c:")
+def read_checksums(checksums_file):
+    checksums = {}      # file -> checksum      -- Same file may appear multiple times in the list. Use the last checksum
+
+    if os.path.isfile(checksums_file):
+        with open(checksums_file, "r") as f:
+            lines = list(f.readlines())
+
+        for line in lines:
+            line = line.strip()
+            if line and not line[0] == "#":
+                path, c0 = line.split(None, 1)
+                checksums[path] = c0.lower()
+    return checksums
+
+def verify(checksums_file):
+    checksums = read_checksums(checksums_file)
+    changed = []
+    missing = []
+    for path, c_old in checksums.items():
+        if not os.path.isfile(path):
+            missing.append(path)
+        else:
+            c = checksum(path)
+            if c_old != c:
+                changed.append((path, c_old, c))
+    return changed, missing
+    
+def update(checksums_file, paths):
+    checksums = read_checksums(checksums_file)
+    paths = set(paths) | set(checksums.keys())
+    for path in paths:
+        checksums[path] = checksum(path)
+    write_checksums(checksums, open(checksums_file, "w"))
+    
+def write_checksums(checksums, out_file):
+    now = datetime.utcnow()
+    print("# use 'python sha256.py -c <this file>' to verify ", file=out_file)
+    print("#", now, "UTC", file=out_file)
+    for path, checksum in sorted(checksums.items()):
+        print(f"{path}\t{checksum}", file=out_file)
+
+opts, args = getopt.getopt(sys.argv[1:], "c:u:v:h?")
 opts = dict(opts)
 
-if not args and not "-c" in opts:
+if "-h" in opts or "-?" in opts or (not opts and not args):
     print(Usage)
     sys.exit(2)
 
 status = 0
 
-if not args:
-    checksums = {}      # file -> checksum      -- Same file may appear multiple times in the list. Use the last checksum
-    lines = open(opts["-c"], "r").readlines()
+if "-v" in opts:
+    changed, missing = verify(opts["-v"])
+    for path, old_c, new_c in changed:
+        print("changed:", path)
+        status = 1
+    for path in missing:
+        print("missing:", path)
+        status = 1
 
-    for line in lines:
-        line = line.strip()
-        if line and not line[0] == "#":
-            path, c0 = line.split(None, 1)
-            checksums[path] = c0.lower()
-
-    for path, c0 in checksums.items():
-        if not os.path.isfile(path):
-            print(f"{path}:\tnot found")
-        else:
-            c1 = checksum(path).lower()
-            if c1 != c0:
-                print(f"{path}:\tchecksum mismatch")
-                status = 1
-            else:
-                print(f"{path}:\tverified")
-                
-                
-elif len(args) == 1:
-    c1 = checksum(args[0]).lower()
-    if "-c" in opts:
-        c0 = opts["-c"].lower()
-        if c0 != c1:
-            print("checksum mismatch for", args[0])
-            status = 1
-    else:
-        print(c1)
-else:
-    now = datetime.utcnow()
-    print("# use 'python sha256.py -c <this file>' to verify ")
-    print("#", now, "UTC")
-    for path in args:
-        c1 = checksum(path).lower()
-        print(f"{path}\t{c1}")
+elif "-u" in opts:
+    update(opts["-u"], args)
     
+elif "-c" in opts:
+    c = opts["-c"]
+    if c != checksum(args[0]):
+        print("changed:", args[0])
+        status = 1
+else:
+    checksums = {}
+    for path in args:
+        checksums[path] = checksum(path)
+    write_checksums(checksums, sys.stdout)
+
 sys.exit(status)
